4018e947fd00167dc0c6d43780b69242b9bed4b3
Rajeev Sharma <talk2rajeev1986@gmail.com>
2023-05-31 00:26:28 +0530

    ch-2 verse 1 to 5

--- src/files/bhagwatGita/chapter3.tsx
+++ src/files/bhagwatGita/chapter3.tsx
@@ -9,121 +9,253 @@ const Chapter3: {[ChapterIndexType.Chapter3]: ChapterInterface } = {
         slokas: [
             {
                 slokaNumber: 1,
-                uvach: "",
-                sanskritSloka: ["", ""],
-                romanSloka: ["", ""],
+                uvach: "अर्जुन उवाच।",
+                sanskritSloka: ["ज्यायसी चेत्कर्मणस्ते मता बुद्धिर्जनार्दन ।", "तत्किं कर्मणि घोरे मां नियोजयसि केशव ।।3।।"],
+                romanSloka: ["jyāyasī cet karmaṇas te matā buddhir janārdana", "tat kiṁ karmaṇi ghore māṁ niyojayasi keśava"],
                 wordsMeaning: [
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "अर्जुनः उवाच",
+                        meaning: "अर्जुन ने कहा",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "ज्यायसी, चेत्",
+                        meaning: "श्रेष्ठ, यदि",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "कर्मण, ते",
+                        meaning: "कर्मफल से, आप द्वारा",
+                    },
+                    {
+                        sanskrit: "मता, बुद्धि",
+                        meaning: "मानना, बुद्धि",
+                    },
+                    {
+                        sanskrit: "जनार्दन",
+                        meaning: "जीवों का पालन करने वाले, श्रीकृष्ण",
+                    },
+                    {
+                        sanskrit: "तत्, किम, कर्मणि",
+                        meaning: "तब, क्यों, कर्म",
+                    },
+                    {
+                        sanskrit: "घोर, मम्",
+                        meaning: "भयंकर, मुझे",
+                    },
+                    {
+                        sanskrit: "नियोजयसि",
+                        meaning: "लगाते हो",
+                    },
+                    {
+                        sanskrit: "केशव",
+                        meaning: "केशी नामक राक्षस का वध करने वाले, श्रीकृष्ण",
                     }
                 ],
-                meaningInHindi: "",
-                meaningInEnglish: '',
+                meaningInHindi: "अर्जुन ने कहा! हे जनार्दन, यदि आप कर्म की अपेक्षा ज्ञान श्रेष्ठ मानते हैं तो फिर हे केशव! मुझे इस भयंकर कर्म में क्यों लगाते हैं?",
+                meaningInEnglish: "Arjuna said: O Janārdana, O Keśava, if it is Your opinion that wisdom is better than action, then why do You wish to engage me in such violent activities?",
                 meaningInGujrati: "",
                 meaningInMarathi: "",
             },
             {
                 slokaNumber: 2,
                 uvach: "",
-                sanskritSloka: ["", ""],
-                romanSloka: ["", ""],
+                sanskritSloka: ["व्यामिश्रेणेव वाक्येन बुद्धिं मोहयसीव मे ।", "तदेकं वद निश्चित्य येन श्रेयोऽहमाप्नुयाम् ॥2॥"],
+                romanSloka: ["vyāmiśreṇeva vākyena buddhiṁ mohayasīva me", "tad ekaṁ vada niścitya yena śreyo’ham āpnuyām"],
                 wordsMeaning: [
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "व्यामिश्रेण",
+                        meaning: "तुम्हारे अनेकार्थक शब्दों का",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "इव, वाक्येन",
+                        meaning: "मानो, वचनों से",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "बुद्धिम्",
+                        meaning: "बुद्धि",
+                    },
+                    {
+                        sanskrit: "मोहयसि",
+                        meaning: "मैं मोहित हो रहा हूँ",
+                    },
+                    {
+                        sanskrit: "इव, मे, तत्",
+                        meaning: "मानो, मेरी, उस",
+                    },
+                    {
+                        sanskrit: "एकम्",
+                        meaning: "एकमात्र",
+                    },
+                    {
+                        sanskrit: "वद",
+                        meaning: "अवगत कराए",
+                    },
+                    {
+                        sanskrit: "निश्चित्य",
+                        meaning: "निश्चित रूप से",
+                    },
+                    {
+                        sanskrit: "येन",
+                        meaning: "जिससे",
+                    },
+                    {
+                        sanskrit: "श्रेयः",
+                        meaning: "अति श्रेष्ठ",
+                    },
+                    {
+                        sanskrit: "अहम्, आप्नुयाम्",
+                        meaning: "मैं, प्राप्त कर सकू",
                     }
                 ],
-                meaningInHindi: "",
-                meaningInEnglish: '',
+                meaningInHindi: "आपके अनेकार्थक वचनों से मेरी बुद्धि भ्रमित हो गयी है। कृपया मुझे निश्चित रूप से कोई एक ऐसा मार्ग बताएँ जिससे मैं कल्याणको प्राप्त कर सकूँ।",
+                meaningInEnglish: "Your words seem to be contradictory and my mind is now confused. Therefore, kindly tell me which particular path is most beneficial for me.",
                 meaningInGujrati: "",
                 meaningInMarathi: "",
             },
             {
                 slokaNumber: 3,
-                uvach: "",
-                sanskritSloka: ["", ""],
-                romanSloka: ["", ""],
+                uvach: "श्रीभगवानुवाच ।",
+                sanskritSloka: ["लोकेऽस्मिन्द्विविधा निष्ठा पुरा प्रोक्ता मयानघ ।", "ज्ञानयोगेन साङ्ख्यानां कर्मयोगेन योगिनाम् ॥3॥"],
+                romanSloka: ["loke’smin dvi-vidhā niṣṭhā purā proktā mayānagha", "jñāna-yogena sāṅkhyānāṁ karma-yogena yoginām"],
                 wordsMeaning: [
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "श्रीभगवान् उवाच",
+                        meaning: "भगवान(श्री कृष्ण) ने कहा",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "लोके, अस्मिन्",
+                        meaning: "संसार में, इस",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "द्वि-विधा, निष्ठा",
+                        meaning: "दो प्रकार की, श्रद्धा",
+                    },
+                    {
+                        sanskrit: "पुरा, प्रोक्ता",
+                        meaning: "पहले, वर्णित",
+                    },
+                    {
+                        sanskrit: "मया",
+                        meaning: "मेरे(श्रीकृष्ण) द्वारा",
+                    },
+                    {
+                        sanskrit: "अनघ",
+                        meaning: "निष्पाप",
+                    },
+                    {
+                        sanskrit: "ज्ञानयोगेन",
+                        meaning: "ज्ञानयोग के द्वारा",
+                    },
+                    {
+                        sanskrit: "सांख्यानाम्",
+                        meaning: "ज्ञानियों का",
+                    },
+                    {
+                        sanskrit: "कर्मयोगेन",
+                        meaning: "कर्म योग के द्वारा",
+                    },
+                    {
+                        sanskrit: "योगिनाम्",
+                        meaning: "योगियों का",
                     }
                 ],
-                meaningInHindi: "",
-                meaningInEnglish: '',
+                meaningInHindi: "भगवान ने कहा, हे निष्पाप अर्जुन! मैं पहले ही दो प्रकार की निष्ठा (मार्गों) का वर्णन कर चुका हूँ। उनमें ज्ञानियों की निष्ठा ज्ञानयोगसे और योगियों की निष्ठा कर्मयोगसे होती है।",
+                meaningInEnglish: "Bhagavān Śrī Kṛṣṇa said: O faultless one, previously I explained the two paths found in this world – the path of wisdom for the empirical philosophers and the path of action for those that must perform action.",
                 meaningInGujrati: "",
                 meaningInMarathi: "",
             },
             {
                 slokaNumber: 4,
                 uvach: "",
-                sanskritSloka: ["", ""],
-                romanSloka: ["", ""],
+                sanskritSloka: ["न कर्मणामनारम्भान्नैष्कर्म्यं पुरुषोऽश्नुते ।", "न च संन्यसनादेव सिद्धिं समधिगच्छति ॥4॥"],
+                romanSloka: ["na karmaṇām anārambhān naiṣkarmyaṁ puruṣo’śnute", "na ca sannyasanād eva siddhiṁ samadhigacchati"],
                 wordsMeaning: [
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "न, कर्मणाम्",
+                        meaning: "नहीं, कर्मों के",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "अनारम्भात्",
+                        meaning: "विमुख रहकर",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "नैष्कर्म्यम्",
+                        meaning: "कर्म-फलों से मुक्ति",
+                    },
+                    {
+                        sanskrit: "पुरुषः, अष्नुते",
+                        meaning: "मनुष्य, प्राप्त करता है;",
+                    },
+                    {
+                        sanskrit: "न, च",
+                        meaning: "नहीं, और",
+                    },
+                    {
+                        sanskrit: "संन्यसनात्",
+                        meaning: "त्याग से",
+                    },
+                    {
+                        sanskrit: "एव",
+                        meaning: "केवल",
+                    },
+                    {
+                        sanskrit: "सिद्धिम्",
+                        meaning: "सफलता",
+                    },
+                    {
+                        sanskrit: "समधि-गच्छति",
+                        meaning: "पा लेता है",
                     }
                 ],
-                meaningInHindi: "",
-                meaningInEnglish: '',
+                meaningInHindi: "मनुष्य न तो कर्म से विमुख रहकर (कर्म न करके) कर्मफल से मुक्ति पा सकता है और न कर्मों के त्यागमात्र से (शारीरिक संन्यास लेकर ) ही वह सिद्धि (पूर्णत्व) को प्राप्त होता है।",
+                meaningInEnglish: "A man can neither get freedom from the fruits of action by remaining aloof from action, nor does he attain the state of divine consciousness  only by renunciation of actions (physical renunciation).",
                 meaningInGujrati: "",
                 meaningInMarathi: "",
             },
             {
                 slokaNumber: 5,
                 uvach: "",
-                sanskritSloka: ["", ""],
-                romanSloka: ["", ""],
+                sanskritSloka: ["न हि कश्चित्क्षणमपि जातु तिष्ठत्यकर्मकृत् ।", "कार्यते ह्यवशः कर्म सर्वः प्रकृतिजैर्गुणैः ॥5॥"],
+                romanSloka: ["na hi kaścit kṣaṇam api jātu tiṣṭhaty-akarmakṛt", "kāryate hy-avaśaḥ karma sarvaḥ prakṛti-jair guṇaiḥ"],
                 wordsMeaning: [
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "न, हि",
+                        meaning: "नहीं, निश्चय ही",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
+                        sanskrit: "कश्चित्, क्षणम्",
+                        meaning: "कोई, क्षण के लिए",
                     },
                     {
-                        sanskrit: "",
-                        meaning: "",
-                    }
+                        sanskrit: "अपि, जातु",
+                        meaning: "भी, सदैव",
+                    },
+                    {
+                        sanskrit: "तिष्ठति, अकर्म",
+                        meaning: "रह सकता है, कृत बिना कर्म",
+                    },
+                    {
+                        sanskrit: "कार्यते, हि",
+                        meaning: "कर्म करने के लिए, निश्चय ही",
+                    },
+                    {
+                        sanskrit: "अवशः",
+                        meaning: "बाध्य होकर",
+                    },
+                    {
+                        sanskrit: "कर्म, सर्वः",
+                        meaning: "कर्म, समस्त",
+                    },
+                    {
+                        sanskrit: "प्रकृति-जैः",
+                        meaning: "प्रकृति के गुणों से उत्पन्न",
+                    },
+                    {
+                        sanskrit: "गुणैः",
+                        meaning: "गुणों के द्वारा",
+                    },
                 ],
-                meaningInHindi: "",
-                meaningInEnglish: '',
+                meaningInHindi: "कोई भी मनुष्य क्षणमात्र  के लिए भी अकर्मा (बिना कर्म किए) नहीं रह सकता। क्योंकि मनुष्य प्रकृतिजन्य गुणों (प्रकृति द्वारा उत्पन्न तीन गुण - सत, रज, तम) के अनुसार कर्म करने के लिए विवश होते हैं।",
+                meaningInEnglish: "No one can remain inaction (without doing action) even for a moment. Indeed, all living beings are forced to engage in action due to the influence of the modes of material nature.",
                 meaningInGujrati: "",
                 meaningInMarathi: "",
             },
